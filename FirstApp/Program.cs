56f02372cbf3938f1f0e6f655daf11ecfcc9e94e
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-08 23:34:27 +0700

    Добавьте файлы проекта.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Jane";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
