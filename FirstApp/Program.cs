96431e48b668cdb473ab74adbd7e17eec0badc02
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-08 23:54:46 +0700

    вывод символа #

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 
 class MainClass
-
 {
-        static void Main(string[] args)
+    static void Main(string[] args)
     {
-        Console.WriteLine("Привет,\n мир!");
+
+        const string MyName = "Jane";
+
+        Console.WriteLine(MyName);
+
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.WriteLine("\u0023");
+        Console.ReadKey();
+
     }
 }
