d7e61b59d5aca7ff21b427a303f7d133e5c3673d
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-08 23:49:50 +0700

    ("Привет,\n мир!")

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 
 class MainClass
+
 {
-    static void Main(string[] args)
+        static void Main(string[] args)
     {
-        const string MyName = "Jane";
-
-        Console.WriteLine(MyName);
-
-        Console.WriteLine("Привет, мир");
-        Console.WriteLine("Мне 27 лет");
-        Console.WriteLine("My name is Jane");
-
-        Console.ReadKey();
+        Console.WriteLine("Привет,\n мир!");
     }
 }
