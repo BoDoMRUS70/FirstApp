becd80cf50027e2d72dc2656455dcd948191badf
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-09 01:32:04 +0700

    Дни недели

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
-
-class MainClass
+﻿namespace FirstApp
 {
-    static void Main(string[] args)
+    enum DaysOfWeek : byte
     {
-        Console.WriteLine("IntMin {0} ", int.MinValue);
-        Console.WriteLine("IntMax {0} ", int.MaxValue);
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
