b37f129a915b9aa4c157d98c621c8ed4f6997d28
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-09 01:00:33 +0700

    обращение в типу int (max и min)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,16 +4,7 @@ class MainClass
 {
     static void Main(string[] args)
     {
-
-        string MyName = "Dima";
-        byte MyAge = 29;
-        bool HaveIApet = true;
-        double MyShoeSize = 41.5;
-
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Console.WriteLine("IntMin {0} ", int.MinValue);
+        Console.WriteLine("IntMax {0} ", int.MaxValue);
     }
 }
