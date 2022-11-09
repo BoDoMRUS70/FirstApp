2c0939295e2f32776679d62c683ef2f9e9e0fd90
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-09 21:50:01 +0700

    Console.WriteLine("Value: {0} Increment: {1}", counter, counter++);

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,8 +5,8 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        double result = 5.0 / 2.0;
-        Console.WriteLine("5 / 2 = {0}", result);
+        int counter = 10;
+        Console.WriteLine("Value: {0} Increment: {1}", counter, ++counter);
 
     }
 }
