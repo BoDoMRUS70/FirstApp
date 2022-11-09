fa420a48a480580289cce5dbd1119298162b4fce
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-09 16:10:29 +0700

    Module 3.5.5"Цыган на цыпочках цыпленку цыкнул цыц"

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -2,11 +2,10 @@
 
 class MainClass
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        string MyName = "Дмитрий";
-        var age = 29;
-        string favcolor = "Black";
-        Console.WriteLine("{0} \n {1} \n {2}", MyName, age, favcolor);
+        
+        string str = Console.ReadLine();
+        
     }
 }
