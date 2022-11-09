e03abd40a9024c37c48307095d2bab3040584ee3
Bodomrus70 <Bodomrus70@mail.ru>
2022-11-09 22:13:18 +0700

    Module 3.6.3

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -5,7 +5,7 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        double result = 5.0 / 2.0 * 5.0;
+        double result = 5.0 / 2.0 * 3.0;
         Console.WriteLine("Value: {0}", result);
 
     }
